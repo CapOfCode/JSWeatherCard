49b9b357251da41c81fd23fbdbb79ea60c3be240
deve-rashed <tools.rashed@gmail.com>
2023-04-03 14:44:42 +0600

    Simple Logic Added to Js File

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-let button = document.querySelector('.searchIcon');
+let button = document.querySelector('.searchIcon').getAttribute('src').add;
 let searchCity = document.getElementById('city');
+let cityNameEl = document.getElementById('cityname');
+let tempEl = document.getElementById('temp'); //temp 
+let humidityEl = document.getElementById('humidity'); //humidity 
+let windEl = document.getElementById('wind'); //wind
 
 button.addEventListener('click', getWeatherData);
 
 function getWeatherData() {
-    let city = searchCity.value
-
+  let city = searchCity.value
   let response = fetch(`https://api.openweathermap.org/data/2.5/weather?appid=44c8cd4cacc8eeaaa0e24c5d38459c68&units=metric&q=${city}`)
   
 
   .then((response) => response.json())
   .then((weatherData) => {
+    let img = weatherData["weather"][0]["main"]
     let temp = weatherData.main.temp;
     let humidity = weatherData.main.humidity
     let cityName = weatherData.name
@@ -21,8 +25,11 @@ function getWeatherData() {
     console.log(humidity);
     console.log(cityName);
     console.log(windSpeed);
+    console.log(img);
 
-
-
+    cityNameEl.innerText = cityName;
+    humidityEl.innerText = humidity;
+    tempEl.innerText = temp;
+    windEl.innerText = windSpeed;
   });
 }
