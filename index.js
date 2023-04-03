b987c6f595d0c995c2aaea83d084e863edd6cc97
deve-rashed <tools.rashed@gmail.com>
2023-04-03 16:55:14 +0600

    JS file Complete

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-let button = document.querySelector('.searchIcon').getAttribute('src').add;
+let button = document.querySelector('.searchIcon');
+let imgEl = document.getElementById('img') //img
 let searchCity = document.getElementById('city');
-let cityNameEl = document.getElementById('cityname');
+let cityNameEl = document.getElementById('cityname'); //cityname
 let tempEl = document.getElementById('temp'); //temp 
 let humidityEl = document.getElementById('humidity'); //humidity 
 let windEl = document.getElementById('wind'); //wind
 
 button.addEventListener('click', getWeatherData);
+document.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    getWeatherData();
+    // document.getElementById("myBtn").click();
+  }
+});
+
+
+
+
 
 function getWeatherData() {
+
   let city = searchCity.value
   let response = fetch(`https://api.openweathermap.org/data/2.5/weather?appid=44c8cd4cacc8eeaaa0e24c5d38459c68&units=metric&q=${city}`)
   
 
-  .then((response) => response.json())
+  .then((response) => {
+    if (response.status == 404 ){
+      alert("City Not Found")
+    }
+    return response.json();
+  })
   .then((weatherData) => {
+
+
+
+
+    let resStatus = response.status;
     let img = weatherData["weather"][0]["main"]
     let temp = weatherData.main.temp;
     let humidity = weatherData.main.humidity
@@ -25,11 +47,21 @@ function getWeatherData() {
     console.log(humidity);
     console.log(cityName);
     console.log(windSpeed);
-    console.log(img);
+    console.log(resStatus);
 
+
+    
     cityNameEl.innerText = cityName;
     humidityEl.innerText = humidity;
     tempEl.innerText = temp;
     windEl.innerText = windSpeed;
+    imgEl.setAttribute("src", img + ".png");
   });
+}
+
+
+//Setting defult value of search input box//
+window.onload = function onloadCity(){
+  document.getElementById("city").value = "Dhaka";
+  getWeatherData();
 }
